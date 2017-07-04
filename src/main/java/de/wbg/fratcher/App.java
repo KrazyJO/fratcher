1b4c75710152bb0c257b0da7516b059ae53a5590
joern.otte@gmx.net <joern.otte@gmx.net>
2017-07-04 19:41:34 +0200

    initial project with modified pom.xml

---
+++ src/main/java/de/wbg/fratcher/App.java
@@ -0,0 +1,13 @@
+package de.wbg.fratcher;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
