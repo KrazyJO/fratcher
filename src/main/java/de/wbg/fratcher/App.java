26acaaaf321a2f8a4ac7af0f71b30d7ab8028300
joern.otte@gmx.net <joern.otte@gmx.net>
2017-07-04 19:46:37 +0200

    modified app start to run spring boot

--- src/main/java/de/wbg/fratcher/App.java
+++ src/main/java/de/wbg/fratcher/App.java
@@ -1,13 +1,42 @@
 package de.wbg.fratcher;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 /**
- * Hello world!
+ * Starts the app
  *
  */
+@EnableSwagger2
+@SpringBootApplication
 public class App 
 {
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
+    }
+    
+    /**
+     * Configuration for Swagger.
+     *
+     * @return a docket.
+     */
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
