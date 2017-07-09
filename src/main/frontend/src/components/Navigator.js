37317b84753ffe2ff2ddc381e6999b3096ab75dc
joern.otte@gmx.net <joern.otte@gmx.net>
2017-07-09 19:54:30 +0200

    extended navbar for sign in elements (not working)

--- src/main/frontend/src/components/Navigator.js
+++ src/main/frontend/src/components/Navigator.js
@@ -7,6 +7,34 @@ class Navigator extends React.Component {
 //        // we would not have to do this.
 //        this.forceUpdate();
 //    }
+	
+	constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            error: undefined
+        };
+
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+//        this.handleLogout = this.handleLogout.bind(this);
+//        this.cookies = this.props.cookies;
+    }
+	
+	handleSubmit(event) {
+		event.preventDefault();
+		console.log("email: " + this.state.email + ", password: " + this.state.password);
+	}
+	
+	handleEmailChange(event) {
+		this.setState({email: event.target.value});
+	}
+	
+	handlePasswordChange(event) {
+		this.setState({password: event.target.value});
+	}
 
     render() {
         return (
@@ -23,8 +51,18 @@ class Navigator extends React.Component {
                         <ul className="nav navbar-nav">
                             <li><Link to="/">Home</Link></li>
                             <li><Link to="/matcher">Matcher</Link></li>
-
                         </ul>
+                        <form className="navbar-form navbar-right" onSubmit={this.handleSubmit}>
+	                        <div className="form-group">
+	                            <input type="text" className="form-control" name="username" placeholder="Username" value={this.state.email}
+                                onChange={this.handleEmailChange}></input>
+	                        </div>
+	                        <div className="form-group">
+	                            <input type="text" className="form-control" name="password" placeholder="Password" value={this.state.password}
+                                onChange={this.handlePasswordChange}></input>
+	                        </div>
+	                        <button type="submit" className="btn btn-success">Sign In</button>
+	                    </form>
                     </div>
                 </div>
             </nav>
