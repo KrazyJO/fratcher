7deb2dd4c28bc510991b4fe62bc2c3ee5040c6a0
joern.otte@gmx.net <joern.otte@gmx.net>
2017-07-09 12:54:56 +0200

    added language detector for i18next and adapted i18n.js to get use of it

--- src/main/frontend/src/i18n.js
+++ src/main/frontend/src/i18n.js
@@ -1,10 +1,12 @@
 import i18n from "i18next";
 import XHR from "i18next-xhr-backend";
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 // See https://www.i18next.com/ for a LOT of documentation.
 
 i18n
     .use(XHR)
+    .use(LanguageDetector)
     .init({
         fallbackLng: 'en',
 
