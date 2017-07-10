59787beb5d15de792ae4f20ac591d688632ac157
joern.otte@gmx.net <joern.otte@gmx.net>
2017-07-10 17:55:17 +0200

    took over authentication from course but without hashing for testing

--- src/main/java/de/wbg/fratcher/user/UserRepository.java
+++ src/main/java/de/wbg/fratcher/user/UserRepository.java
@@ -1,7 +1,12 @@
 package de.wbg.fratcher.user;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
+	@Query("SELECT u from User_ u WHERE u.email = :email AND u.password = :password")
+    User findByEmailAndPassword(@Param("email") String email, @Param("password") String password);
 }
--- src/main/java/de/wbg/fratcher/user/UserService.java
+++ src/main/java/de/wbg/fratcher/user/UserService.java
@@ -1,9 +1,17 @@
 package de.wbg.fratcher.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+	@Autowired
+	private UserRepository userRepository;
 	
+	public User getUser(String mail, String password)
+	{
+		User user = userRepository.findByEmailAndPassword(mail, password);
+		return user;
+	}
 }
