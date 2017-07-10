87b4d3df7cb9b9efe6c99e2913261bd7d0a03b83
joern.otte@gmx.net <joern.otte@gmx.net>
2017-07-10 17:35:39 +0200

    added h2 database and added update sql on startup

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,13 @@
+-- Initialize database on startup. See
+-- https://docs.spring.io/spring-boot/docs/current/reference/html/howto-database-initialization.html#howto-initialize-a-database-using-spring-jdbc
+-- for explanation. This is a cool spring feature :-).
+
+
+-- Remove everything.
+DELETE FROM USER_;
+
+
+-- Insert new users.
+INSERT INTO USER_ (id, email, PASSWORD) VALUES
+  (1, 'bob', 'bar'), 
+  (2, 'sally', 'foo');
