9a60b09ff4b55306a82a0ba23835e3b94ea90aa7
joern.otte@gmx.net <joern.otte@gmx.net>
2017-07-10 18:48:46 +0200

    server sets cookie 'auth' on successful authentication

--- src/main/java/de/wbg/fratcher/authentication/AuthenticationController.java
+++ src/main/java/de/wbg/fratcher/authentication/AuthenticationController.java
@@ -1,6 +1,10 @@
 package de.wbg.fratcher.authentication;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,13 +23,17 @@ public class AuthenticationController {
     }
 	
 	@RequestMapping(value = "/api/user/login", method = RequestMethod.POST)
-    public ResponseEntity<AuthenticationService.UserToken> login(@RequestBody UserLogin userLogin) {
+    public ResponseEntity<AuthenticationService.UserToken> login(@RequestBody UserLogin userLogin, HttpServletResponse response) {
         AuthenticationService.UserToken token = authenticationService.login(userLogin.userName, userLogin.password);
 
         if (token == null) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
-        return new ResponseEntity<>(token, HttpStatus.OK);
+//        response.addCookie(new Cookie("auth", "bearer " + token.token));
+        
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Set-Cookie", "auth=bearer " + token.token);
+        return new ResponseEntity<>(token,  headers, HttpStatus.OK);
     }
 	
 }
