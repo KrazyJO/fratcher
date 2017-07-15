a1d675848d649d9e8d8bcf1a9d1736610553362f
joern.otte@gmx.net <joern.otte@gmx.net>
2017-07-15 12:02:09 +0200

    extended register comp with html elements and empty methods

--- src/main/frontend/src/components/Register.js
+++ src/main/frontend/src/components/Register.js
@@ -7,13 +7,48 @@ class Register extends React.Component {
         this.state = {
         };
         
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleuserNameChange = this.handleuserNameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handlePasswordRepeatChange = this.handlePasswordRepeatChange.bind(this);
     }
 
+    handleSubmit() {
+    	console.log("handleSubmit register");
+    }
+    
+    handleuserNameChange(event) {
+    	
+    }
+    
+    handlePasswordChange(event) {
+    	
+    }
+    
+    handlePasswordRepeatChange(event) {
+    	
+    }
+    
     render () {
     	const {t} = this.props;
 
     	return (
-    			<div className="component">{t('registerComp')}</div>
+    			<div>
+	    			<div className="component">{t('registerComp')}</div>
+	    			<form onSubmit={this.handleSubmit}>
+				        <div>
+					        <input type="text" placeholder={t('user')} value={this.state.userName}
+					        onChange={this.handleuserNameChange}></input>
+					    </div>
+					    <div>
+					        <input type="text" type="password" placeholder={t('password')} value={this.state.password}
+					        onChange={this.handlePasswordChange}></input>
+					        <input type="text" type="password" placeholder={t('passwordRepeat')} value={this.state.passwordRepeat}
+					        onChange={this.handlePasswordRepeatChange}></input>
+					    </div>
+					    <button type="submit">{t('sendSignIn')}</button>
+			    	</form>	
+			    </div>
     	);
     }
 }
