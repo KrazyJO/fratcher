a5153db0d40c500115187f91a07c8912696abdac
joern.otte@gmx.net <joern.otte@gmx.net>
2017-07-15 12:09:57 +0200

    styling register with bootstrap and own css

--- src/main/frontend/src/components/Register.js
+++ src/main/frontend/src/components/Register.js
@@ -33,20 +33,21 @@ class Register extends React.Component {
     	const {t} = this.props;
 
     	return (
-    			<div>
-	    			<div className="component">{t('registerComp')}</div>
-	    			<form onSubmit={this.handleSubmit}>
-				        <div>
-					        <input type="text" placeholder={t('user')} value={this.state.userName}
-					        onChange={this.handleuserNameChange}></input>
+    			<div className="center width500 main-login main-center">
+	    			<form className="form-horizontal" onSubmit={this.handleSubmit}>
+				        <div className="form-group">
+					        <input className="form-control" type="text" placeholder={t('user')} value={this.state.userName}
+					        	onChange={this.handleuserNameChange}></input>
 					    </div>
-					    <div>
-					        <input type="text" type="password" placeholder={t('password')} value={this.state.password}
-					        onChange={this.handlePasswordChange}></input>
-					        <input type="text" type="password" placeholder={t('passwordRepeat')} value={this.state.passwordRepeat}
-					        onChange={this.handlePasswordRepeatChange}></input>
+					    <div className="form-group">
+					        <input className="form-control" type="text" type="password" placeholder={t('password')} value={this.state.password}
+					        	onChange={this.handlePasswordChange}></input>
 					    </div>
-					    <button type="submit">{t('sendSignIn')}</button>
+					    <div className="form-group">
+						    <input className="form-control" type="text" type="password" placeholder={t('passwordRepeat')} value={this.state.passwordRepeat}
+					        	onChange={this.handlePasswordRepeatChange}></input>
+					    </div>
+					    <button type="submit" className="width500 btn btn-success">{t('sendSignIn')}</button>
 			    	</form>	
 			    </div>
     	);
