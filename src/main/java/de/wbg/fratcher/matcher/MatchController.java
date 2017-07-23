2f4b48b3f2a2f0eec25118ac1aa912b34818687e
joern.otte@gmx.net <joern.otte@gmx.net>
2017-07-23 11:37:21 +0200

    return profile in /api/matches instead of user

--- src/main/java/de/wbg/fratcher/matcher/MatchController.java
+++ src/main/java/de/wbg/fratcher/matcher/MatchController.java
@@ -1,17 +1,14 @@
 package de.wbg.fratcher.matcher;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import de.wbg.fratcher.profile.Profile;
 import de.wbg.fratcher.user.User;
-import de.wbg.fratcher.user.UserRepository;
 import de.wbg.fratcher.user.UserService;
 
 @RestController
@@ -24,7 +21,7 @@ public class MatchController {
 	private UserService userService;
 	
 	@RequestMapping(value = "/api/matches")
-	public ResponseEntity<Iterable<User>> getUsersMatched() 
+	public ResponseEntity<LinkedList<Profile>> getUsersMatched() 
 	{
 		if (userService.isAnonymous())
 		{
@@ -33,8 +30,13 @@ public class MatchController {
 		
 		Long userId = userService.getCurrentUser().getId();
 		Iterable<User> userMatches = matchService.findUserMatches(userId);
+		LinkedList<Profile> userProfiles = new LinkedList<>();
+		for (User u : userMatches)
+		{
+			userProfiles.push(u.getProfile());
+		}
 		
-		return ResponseEntity.ok(userMatches);
+		return ResponseEntity.ok(userProfiles);
 	}
 	
 }
