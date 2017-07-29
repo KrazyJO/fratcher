4e749aa8f4ae018a7f710b23079d2ae1932a7510
joern.otte@gmx.net <joern.otte@gmx.net>
2017-07-29 15:11:34 +0200

    implemented get messages between 2 specified users

--- src/main/java/de/wbg/fratcher/chat/ChatController.java
+++ src/main/java/de/wbg/fratcher/chat/ChatController.java
@@ -1,5 +1,8 @@
 package de.wbg.fratcher.chat;
 
+import java.util.Iterator;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,14 +17,17 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @RestController
 public class ChatController implements WebSocketConfigurer {
 
+	@Autowired
+	private MessageRepository messageRepository;
+	
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
 		registry.addHandler(new ChatHandler(), "/api/chat");
 	}
 	
-	@RequestMapping(value = "/api/chat/{userId}", method = RequestMethod.GET)
-	public void getAllChatEntriesForUser(@PathVariable Long userId) {
-		
+	@RequestMapping(value = "/api/chat/{userIdOne}/{userIdTwo}", method = RequestMethod.GET)
+	public Iterable<Message> getAllChatEntriesForUser(@PathVariable Long userIdOne, @PathVariable Long userIdTwo) {
+		return messageRepository.findMessagesForUser(1L, 2L);
 	}
 
 }
--- src/main/java/de/wbg/fratcher/chat/MessageRepository.java
+++ src/main/java/de/wbg/fratcher/chat/MessageRepository.java
@@ -1,13 +1,13 @@
 package de.wbg.fratcher.chat;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-import de.wbg.fratcher.user.User;
-
 public interface MessageRepository extends CrudRepository<Message, Long>{
 
-	@Query("SELECT m from Message m WHERE m.userIdFrom = :userIdPartnerOne AND m.userIdTo = :userIdPartnerTwo")
-    User findMessagesForUser(@Param("userIdPartnerOne") Long userIdPartnerOne, @Param("userIdPartnerTwo") Long userIdPartnerTwo);
+	@Query("SELECT m from Message m WHERE m.userIdFrom = :userIdPartnerOne AND m.userIdTo = :userIdPartnerTwo or m.userIdFrom = :userIdPartnerTwo AND m.userIdTo = :userIdPartnerOne")
+    List<Message> findMessagesForUser(@Param("userIdPartnerOne") Long userIdPartnerOne, @Param("userIdPartnerTwo") Long userIdPartnerTwo);
 }
