3f4b05c07e442d9aa4375f8f4e0acbb2faa9ae52
joern.otte@gmx.net <joern.otte@gmx.net>
2017-08-13 14:57:16 +0200

    order for messages is now correct; removed not used method

--- src/main/java/de/wbg/fratcher/chat/MessageRepository.java
+++ src/main/java/de/wbg/fratcher/chat/MessageRepository.java
@@ -8,11 +8,8 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface MessageRepository extends CrudRepository<Message, Long>{
-
-	@Query("SELECT m from Message m WHERE (m.userIdFrom = :userIdPartnerOne AND m.userIdTo = :userIdPartnerTwo) or (m.userIdFrom = :userIdPartnerTwo AND m.userIdTo = :userIdPartnerOne)")
-    List<Message> findMessagesForUser(@Param("userIdPartnerOne") Long userIdPartnerOne, @Param("userIdPartnerTwo") Long userIdPartnerTwo);
 	
-	@Query("SELECT m FROM Message m WHERE m.userIdFrom in (:userList) and m.userIdTo in (:userList)")
+	@Query("SELECT m FROM Message m WHERE m.userIdFrom in (:userList) and m.userIdTo in (:userList) ORDER BY m.createdAt ASC")
 	List<Message> findMessagesForUserByList(@Param("userList") ArrayList<Long> userList);
 	
 	@Query("SELECT COUNT(m) FROM Message m WHERE m.userIdTo = :userId and m.userIdFrom = :userIdFrom and m.read = false")
