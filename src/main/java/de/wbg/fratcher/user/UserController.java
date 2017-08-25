79ac6c74385a75104e33d61a772b7275a10027fe
joern.otte@gmx.net <joern.otte@gmx.net>
2017-08-25 17:35:45 +0200

    added requestmethod post to /api/user/create

--- src/main/java/de/wbg/fratcher/user/UserController.java
+++ src/main/java/de/wbg/fratcher/user/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.wbg.fratcher.authentication.AuthenticationService;
@@ -29,7 +30,7 @@ public class UserController {
 	@Autowired
 	private AuthenticationService authenticationService;
 	
-	@RequestMapping(value = "/api/user/create")
+	@RequestMapping(value = "/api/user/create", method = RequestMethod.POST)
 	public ResponseEntity<AuthenticationService.UserToken> userCreate(@RequestBody UserCreate user, HttpServletResponseWrapper response) 
 	{
 		User u = new User();
